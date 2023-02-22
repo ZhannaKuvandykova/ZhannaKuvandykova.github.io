6c2124364aa7de57e8bb9c008bb2e1a7954c4328
Zhanna Kuvandykova <zhannakuvandykova@MacBook-Air-Zhanna.local>
2023-02-22 10:44:31 +0600

    lab5

--- lab5/main.js
+++ lab5/main.js
@@ -1,7 +1,7 @@
 let column_1 = [];
 let column_2 = [];
 let column_3 = [];
-let column_4 = []
+let column_4 = [];
 let sum_1 = 0;
 let sum_2 = 0;
 let sum_3 = 0;
@@ -11,8 +11,8 @@ let sum_4 = 0;
 for (let i=0; i<30; i++) {
  column_1.push(Math.floor(Math.random()*10)+1)
  column_2.push(Math.floor(Math.random()*10)+1)
- column_3.push(Math.floor(Math.random()*10)+1)
- column_4.push(Math.floor(Math.random()*10)+1)
+ column_3.push(Math.floor(Math.random()*100)+1)
+ column_4.push(Math.floor(Math.random()*100)+1)
 }
 
 for (let i=0; i<30; i++) {
@@ -22,7 +22,14 @@ for (let i=0; i<30; i++) {
  sum_4 = sum_4 + column_4[i];
 }
 
-console.log(sum_1)
-console.log(sum_2)
-console.log(sum_3)
-console.log(sum_4)
+let sums = [sum_1, sum_2, sum_3, sum_4];
+let total_score = sum_1 + sum_2 + sum_3 + sum_4;
+const results = {
+    column_1,
+    column_2,
+    column_3,
+    column_4,
+    sums,
+    total_score,
+}
+console.log(results);
