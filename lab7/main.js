ee7b60ff5eca7aff9beef5886d76ba2918eed680
Zhanna Kuvandykova <zhannakuvandykova@MacBook-Air-Zhanna.local>
2023-03-17 10:37:54 +0600

    lab7

--- lab7/main.js
+++ lab7/main.js
@@ -12,13 +12,14 @@ const body = document.querySelector("body");
 body.appendChild(div1);
 
 const task = document.createElement('h2');
+
 task.textContent = 'Task';
 task.style.color = 'red';
 
 div1.appendChild(task);
 
 const kek = document.createElement('p');
-kek.textContent = 'In this task you have to reproduce this HTML page as is using only JavaScript. The task will be evaluated on the basis of the following DOM manipulation techniques:'
+kek.innerHTML = 'In this task you have to reproduce this HTML page as is using <b>only</b> JavaScript. The task will be evaluated on the basis of the following DOM manipulation techniques:'
 
 div1.appendChild(kek);
 
@@ -27,38 +28,38 @@ const list1 = document.createElement('ul');
 div1.appendChild(list1);
 
 const l1 = document.createElement('li');
-l1.textContent = 'find elements in the DOM (5 points);'
+l1.innerHTML = 'find elements in the DOM <b>(5 points);</b>'
 l1.style.color ='green';
 
 list1.appendChild(l1);
 
 const l2 = document.createElement('li');
-l2.textContent = 'create/add/remove elements (5 points);'
+l2.innerHTML = 'create/add/remove elements <b>(5 points);</b>'
 l2.style.color ='purple';
 
 list1.appendChild(l2);
 
 
 const l3 = document.createElement('li');
-l3.textContent = 'change content of the elements (5 points);'
+l3.innerHTML = 'change content of the elements <b>(5 points);</b>'
 l3.style.color ='green';
 
 list1.appendChild(l3);
 
 const l4 = document.createElement('li');
-l4.textContent = 'change styles of the elements (5 points);'
+l4.innerHTML = 'change styles of the elements <b>(5 points);</b>'
 l4.style.color ='purple';
 
 list1.appendChild(l4);
 
 const l5 = document.createElement('li');
-l5.textContent = 'change attributes of the elements (5 points);'
+l5.innerHTML = 'change attributes of the elements <b>(5 points);</b>'
 l5.style.color ='green';
 
 list1.appendChild(l5);
 
 const l6 = document.createElement('li');
-l6.textContent = 'change classes of the elements (5 points).'
+l6.innerHTML = 'change classes of the elements <b>(5 points).</b>'
 l6.style.color ='purple';
 
 list1.appendChild(l6);
@@ -86,7 +87,7 @@ const list2 = document.createElement('ul');
 div2.appendChild(list2);
 
 const l1_1 = document.createElement('li');
-l1_1.textContent = 'Create a new repository on Github, named lab7 (1 point).'
+l1_1.innerHTML = 'Create a new repository on Github, named <b>lab7 (1 point).</b>'
 l1_1.style.color ='green';
 
 list2.appendChild(l1_1);
@@ -99,31 +100,31 @@ l1_2.style.color ='purple';
 list2.appendChild(l1_2);
 
 const l1_3 = document.createElement('li');
-l1_3.textContent = 'Create a new HTML file, called index.html, which has only one <h1> tag with "Hello, World!" message (1 point).'
+l1_3.innerHTML = 'Create a new HTML file, called <b>index.html</b>, which has only one &lt;h1&gt; tag with &quot;Hello, World!&quot; message <b>(1 point).</b>'
 l1_3.style.color ='green';
 
 list2.appendChild(l1_3);
 
 const l1_4 = document.createElement('li');
-l1_4.textContent = 'Create a new JavaScript file, called main.js, which must contain your program (assignment) described above (1 point).'
+l1_4.innerHTML = 'Create a new JavaScript file, called <b>main.js</b>, which must contain your program (assignment) described above <b>(1 point).</b>'
 l1_4.style.color ='purple';
 
 list2.appendChild(l1_4);
 
 const l1_5 = document.createElement('li');
-l1_5.textContent = 'Link this main.js file to your index.html file (Note: place your script at the end of the body section).'
+l1_5.innerHTML = 'Link this <b>main.js</b> file to your <b>index.html</b> file (Note: place your script at the end of the <b>body</b> section).'
 l1_5.style.color ='green';
 
 list2.appendChild(l1_5);
 
 const l1_6 = document.createElement('li');
-l1_6.textContent = 'Write a JavaScript program in main.js to make your index.html look identical to this HTML file (5 points).'
+l1_6.innerHTML = 'Write a JavaScript program in <b>main.js</b> to make your <b>index.html</b> look identical to this HTML file <b>(5 points).</b>'
 l1_6.style.color ='purple';
 
 list2.appendChild(l1_6);
 
 const l1_7 = document.createElement('li');
-l1_7.textContent = 'After you finish your work, submit it to the Github (2 points).'
+l1_7.innerHTML = 'After you finish your work, submit it to the Github <b>(2 points).</b>'
 l1_7.style.color ='green';
 
 list2.appendChild(l1_7);
